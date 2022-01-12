a8f055373aa20dd58f5de74d18dffe7f6e78fb0e
BuildTools <sarquentin@gmail.com>
2022-01-12 10:59:33 +0100

    Updating sitemap generator command

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -46,7 +46,9 @@ class GenerateSitemap extends Command
                 ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
                 ->setPriority(0.7))
             ->add(Url::create('/dataactu')
-                ->setLastModificationDate(Carbon::createFromTimeString(Article::published() != null ? Article::published()->with('categories')
+                ->setLastModificationDate(Carbon::createFromTimeString(Article::published()->whereHas('categories', function($query) {
+                    $query->where('name', 'LIKE', 'DataActu');
+                    }) != null ? Article::published()->with('categories')
                     ->whereHas('categories', function($query) {
                         $query->where('name', 'LIKE', 'DataActu');
                     })->orderBy('published_at', 'desc')->first()->published_at : Article::published()->orderBy('published_at', 'desc')->first()->published_at))
