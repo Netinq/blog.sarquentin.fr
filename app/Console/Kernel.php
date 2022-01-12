be8cb981418005993b25cbad91e93eaafa670238
BuildTools <sarquentin@gmail.com>
2022-01-12 10:41:42 +0100

    Fixing shedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('sitemap:generate')->daily();
+        $schedule->command('sitemap:generate')->daily('01:00')->timezone('Europe/Paris');
     }
 
     /**
