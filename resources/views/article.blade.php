b919ed796493ebe13300ac85c73ccc977ca6b9cc
BuildTools <sarquentin@gmail.com>
2022-08-31 01:28:14 +0200

    Adding banner image

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     @include('components.share', ['txt' => $article->name, 'url' => route('article', ['link' => $article->link])])
     <article id="page">
-        <header id="article-head" style="background-image: url('{{asset('storage/'.str_replace('\\', '/', $article->image))}}')">
+        <header id="article-head" style="background-image: url('{{asset('storage/'.str_replace('\\', '/', $article->banner_image))}}')">
             <div>
                 <time class="published" datetime="{{$article->published_at}}">Publication le {{\Carbon\Carbon::parse($article->published_at)->translatedFormat('d F Y')}}</time>
                 <h1>{{$article->name}}</h1>
