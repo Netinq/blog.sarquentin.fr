69c62744f1b2385909edd02306678107e43b2104
BOOK-QU8VNSMR86\sarqu <comptes@sarquentin.fr>
2023-05-15 20:16:38 +0200

    :bug: Fixing SEO issues

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,12 +1,13 @@
 <header id="menu">
     <div class="container">
         <a href="{{route('home')}}">
-            <img src="{{asset('img/logo.png')}}" alt="Logo du site">
+            <img src="{{asset('img/logo.png')}}" alt="Logo du site" height="75px" width="75px">
         </a>
         <div class="nav">
             <form action="{{route('search')}}" method="GET">
                 <input type="search" name="search" placeholder="Rechercher..." value="@isset($search){{$search}}@endisset">
-                <button type="submit"><img src="{{asset('img/search.svg')}}" alt="Search icon"></button>
+                <button type="submit"><img src="{{asset('img/search.svg')}}" alt="Search icon" height="25px"
+                                           width="25px"></button>
             </form>
 {{--            <button type="button"><img src="{{asset('img/burger.svg')}}" alt="Burger menu icon"></button>--}}
         </div>
